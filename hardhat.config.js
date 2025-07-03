6822909351885aa8913daf16336554d69d600423
megmontanez2000 <megmontanez2000@yahoo.com>
2025-07-02 22:06:51 -0400

    Update hardhat config for HNT Sepolia deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.28",
   networks: {
     sepolia: {
       url: "https://sepolia.infura.io/v3/dbafc1c5e20d4d849e9b625fc28c4154",
